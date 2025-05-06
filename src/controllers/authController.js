2cb25481caab3a102c1d1cc4a21f923844255859
techcomkathia <techcomk@gmail.com>
2025-05-06 18:31:48 -0300

    base arquivos

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,31 +5,7 @@ const userService = require('../services/userService');
 const secret = 'seusegredoaqui'; // Idealmente, use variável de ambiente
 
 const login = async (req, res) => {
-  const { userName, password } = req.body;
-
-  try {
-    const user = await userService.getUserByUserName(userName);
-
-    if (!user) {
-      return res.status(401).json({ message: 'Usuário não encontrado' });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Senha inválida' });
-    }
-
-    const token = jwt.sign(
-      { userId: user.id, userName: user.userName },
-      secret,
-      { expiresIn: '1h' }
-    );
-
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ message: 'Erro no login', error });
-  }
+ 
 };
 
 module.exports = { login };
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,16 +3,7 @@ const jwt = require('jsonwebtoken');
 const secret = 'seusegredoaqui'; // De preferência, use variável de ambiente
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) return res.status(401).json({ message: 'Token não fornecido' });
-
-  jwt.verify(token, secret, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Token inválido' });
-    req.user = user;
-    next();
-  });
+  
 };
 
 module.exports = authenticateToken;
